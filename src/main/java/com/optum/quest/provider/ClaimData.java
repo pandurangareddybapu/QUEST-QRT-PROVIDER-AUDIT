3516d54707cb40e25bc9e6e8fd9f30f58030a7a2
pandurangareddybapu <62104809+pandurangareddybapu@users.noreply.github.com>
2020-04-15 18:10:06 +0530

    new class file

---
+++ src/main/java/com/optum/quest/provider/ClaimData.java
@@ -0,0 +1,7 @@
+public class ClaimData {
+
+        public static void main(String[] args){
+            String Name="Provider Data Audit";
+            System.out.println("Quest QRT application  "+Name);
+        }
+    }
