c83955ebf8889311c0e0c915ee318d48cc6f57ff
pandurangareddybapu <pandurangareddybapu@gmail.com>
2020-04-15 18:37:46 +0530

    Added claim data

--- src/main/java/com/optum/quest/provider/ClaimData.java
+++ src/main/java/com/optum/quest/provider/ClaimData.java
@@ -1,7 +1,14 @@
+
+package com.optum.quest.provider;
+
 public class ClaimData {
 
         public static void main(String[] args){
             String Name="Provider Data Audit";
             System.out.println("Quest QRT application  "+Name);
+            System.out.println("Quest QRT application  "+Name);
+            System.out.println("Quest QRT application  "+Name);
+            System.out.println("Quest QRT application  "+Name);
+
         }
     }
